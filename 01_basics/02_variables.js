c9afb59e93859c24380b335a5d5fcbde79588738
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-11-26 09:46:27 +0530

    day- 2

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,31 @@
+"use strick"; // treat all JS code as newer version
+//alert(3+3) we are useing node js, not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+
+console.log("somu")
+
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof "somu")
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
