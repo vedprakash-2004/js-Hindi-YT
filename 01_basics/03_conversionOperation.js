e79d97a07609c4eda4fe41dbb7cf03496fb4dcbd
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-11-28 09:45:28 +0530

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+//"" => false
+//"somu" => true
+
+let isLoggedIn = "somu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
