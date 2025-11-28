b4fce0cb0c311532c190a6cf934c5eca8c7c3906
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-11-28 19:13:38 +0530

    dataType Summary

---
+++ 01_basics/dataTypessummary.js
@@ -0,0 +1,41 @@
+// primitive 
+
+// 7 types : String, Number, Boolean,  null, undefined, Symbol,BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id === anotherId);
+
+
+// const bigNumber = 345584345435435435n
+
+
+// Reference (Non primitive)
+
+
+// Arrys objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+
+    name: "somu",
+    age: 22,
+}
+
+
+const myFunction = function (){
+    console.log("Hello Wdorld");
+}
+
+console.log(typeof bigNumber );
+
+
