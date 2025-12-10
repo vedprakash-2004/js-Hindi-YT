ead8d183b26faa7c1447971d93831e6a9954a102
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-12-10 08:21:57 +0530

    scope2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,9 +3,48 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER:", a);
+    // console.log("INNER:", a);
 }
 
-console.log(a);
+// console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username = "somu"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+    }
+    // console.log(website);
+
+    two()
+}
+// one()
+
+if(true){
+    const username = "somu"
+    if(username === "somu"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+addone(5)
+
+const addTwo = function(num){
+    return num + 2
+
+}
+addTwo(5)
