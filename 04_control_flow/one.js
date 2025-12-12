0940187f6cc53b5670fcc8389dda409e735f4ace
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-12-12 10:01:41 +0530

    conditional statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,49 @@
+// if
+
+// const isUserloggedIn = true
+
+// if( 2 == "2"){
+//     console.log("executed");
+// }
+// console.log("Executed");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2"); //inmature code
+
+if(balance < 500){
+    console.log("Less than 500");
+}else if(balance <750){
+    console.log("less than 750");
+}else if(balance < 900){
+    console.log("less than 750");
+}
+else{
+    console.log("less than 750");
+}
+
+const isUserloggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(isUserloggedIn && debitcard && 2==3){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,45 @@
+// switch (key){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 14
+
+switch(month) {
+    case 1:
+        console.log("January");
+        break;
+
+    case 2:
+        console.log("febrary");
+        break;
+
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("August");
+        break;
+
+
+
+    default:
+        console.log("default case match");
+        break;
+}
