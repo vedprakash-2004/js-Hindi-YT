8fd38b6cdfd0598807c8f4f452d3efaac1215fae
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-12-13 10:07:49 +0530

    while, do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let i = 0; 
+// while(i <= 10){
+//     console.log(`value of index is ${i}`);
+//     i = i+2;
+// }
+
+
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while(arr<myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr+1;
+// }
+
+
+let score = 1
+
+do{
+    console.log(`score is ${score}`);
+    score++;
+}while(score <= 10);
